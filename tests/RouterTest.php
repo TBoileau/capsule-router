1c04cc1e5d4832ca619386306041a4f613e167a9
Thomas Boileau <t-boileau@email.com>
2020-05-21 00:19:17 +0200

    Feature/optional arg (#2)
    * refactor(test): refactor router test
    
    Rename tests in `RouterTest`.
    Add provider for `RouterTest::test if route retrieves good action` to make it easier.
    
    * test(lib): test route with optional arg
    
    Add unit test to check if optional arg in route's path return default value.
    
    * fix(test): missing slash
    
    Add slash to fix wrong path.
    
    * fix(test): delete useless test
    
    Delete `test if route not found by get`
    
    * refactor(lib): delete dead method
    
    Delete method `get` in `Router`.
    
    * feat(lib): add optional arg
    
    Add optional arg in route by defining one or multiple default parameters.
    
    * ci(lib): update phpcs command
    
    Remove `tests` folder from **phpcs** command.
    
    * style(lib): fix coding style errors
    
    Fix **PSR12** coding style errors.

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -2,6 +2,7 @@
 
 namespace TBoileau\Router\Tests;
 
+use Generator;
 use PHPUnit\Framework\TestCase;
 use TBoileau\Router\Route;
 use TBoileau\Router\RouteAlreadyExistsException;
@@ -17,66 +18,91 @@ use TBoileau\Router\Tests\Fixtures\HomeController;
  */
 class RouterTest extends TestCase
 {
-    public function test()
+    /**
+     * @var Router
+     */
+    private Router $router;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
     {
-        $router = new Router();
-
-        $routeHome = new Route("home", "/", [HomeController::class, "index"]);
-
-        $routeFoo = new Route("foo", "/foo/{bar}", [FooController::class, "bar"]);
-
-        $routeArticle = new Route(
-            "article",
-            "/blog/{id}/{slug}",
-            function (string $slug, string $id) {
-                return sprintf("%s : %s", $id, $slug);
-            }
-        );
-
-        $router->add($routeHome);
-        $router->add($routeFoo);
-        $router->add($routeArticle);
-
-        $this->assertCount(3, $router->getRouteCollection());
-
-        $this->assertContainsOnlyInstancesOf(Route::class, $router->getRouteCollection());
-
-        $this->assertEquals($routeHome, $router->get("home"));
-
-        $this->assertEquals($routeHome, $router->match("/"));
-        $this->assertEquals($routeArticle, $router->match("/blog/12/mon-article"));
-
-        $this->assertEquals("Hello world !", $router->call("/"));
-
-        $this->assertEquals(
-            "12 : mon-article",
-            $router->call("/blog/12/mon-article")
-        );
-
-        $this->assertEquals(
-            "bar",
-            $router->call("/foo/bar")
-        );
+        $this->router = new Router();
     }
 
-    public function testIfRouteNotFoundByMatch()
+    /**
+     * @dataProvider provideRoutes
+     * @param Route $route
+     * @param string $path
+     * @param string $response
+     * @throws RouteAlreadyExistsException
+     * @throws RouteNotFoundException
+     * @throws \ReflectionException
+     */
+    public function test if route retrieves good action(Route $route, string $path, string $response)
     {
-        $router = new Router();
+        $this->router->add($route);
+
+        $this->assertContains($route, $this->router->getRouteCollection());
+
+        $this->assertEquals($route, $this->router->match($path));
+
+        $this->assertStringContainsString($response, $this->router->call($path));
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideRoutes(): Generator
+    {
+        yield [
+            new Route("home", "/", [HomeController::class, "index"]),
+            "/",
+            "Hello world !"
+        ];
+
+        yield [
+            new Route("foo", "/foo/{bar}", [FooController::class, "bar"]),
+            "/foo/test",
+            "test"
+        ];
+
+        yield [
+            new Route(
+                "article",
+                "/blog/{id}/{slug}",
+                function (string $slug, string $id) {
+                    return sprintf("%s : %s", $id, $slug);
+                }
+            ),
+            "/blog/12/article",
+            "12 : article"
+        ];
+
+        yield [
+            new Route(
+                "blog",
+                "/blog/{page}",
+                function (int $page) {
+                    return sprintf("Page %d", $page);
+                },
+                ["page" => 1]
+            ),
+            "/blog/",
+            "Page 1"
+        ];
+    }
+
+    public function test if route not found by match()
+    {
         $this->expectException(RouteNotFoundException::class);
-        $router->match("/");
+        $this->router->match("/");
     }
 
-    public function testIfRouteNotFoundByGet()
+    public function test if route already exists()
     {
-        $router = new Router();
-        $this->expectException(RouteNotFoundException::class);
-        $router->get("fail");
-    }
-
-    public function testIfRouteAlreadyExists()
-    {
-        $router = new Router();
-        $router->add(
+        $this->router->add(
             new Route(
                 "home",
                 "/",
@@ -85,7 +111,7 @@ class RouterTest extends TestCase
             )
         );
         $this->expectException(RouteAlreadyExistsException::class);
-        $router->add(
+        $this->router->add(
             new Route(
                 "home",
                 "/",
